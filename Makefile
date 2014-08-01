27ed36149303c2528d229a6bacd8b7d8cf80d8b2
xiaofg <xiaofg@xiaofg-N43JF.(none)>
2014-08-01 23:17:52 +0800

    Add makefile and rebar.config. Compiled and ran success.

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	test -d deps || rebar get-deps
+	rebar compile
+	@erl -noshell -pa './deps/bitcask/ebin' -pa './ebin' -s surefreeinggo start
--- src/surefreeinggo.erl
+++ src/surefreeinggo.erl
@@ -2,4 +2,19 @@
 -export([start/0]).
 
 start() ->
-	io:format("Hello my name is surefreeinggo~n").
+	io:format("Hello my name is surefreeinggo~n"),
+	Handle = bitcask:open("surefreeinggo_db", [read_write]),
+	N = fetch(Handle),
+	store(Handle, N+1),
+	io:format("surefreeinggo has been run ~p times~n", [N]),
+	bitcask:close(Handle),
+	init:stop().
+
+store(Handle, N) ->
+	bitcask:put(Handle, <<"surefreeinggo_executions">>, term_to_binary(N)).
+
+fetch(Handle) ->
+	case bitcask:get(Handle, <<"surefreeinggo_executions">>) of 
+		not_found -> 1;
+		{ok, Bin} -> binary_to_term(Bin)
+	end.
